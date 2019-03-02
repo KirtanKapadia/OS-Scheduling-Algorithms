f9c260e95fc023479aa5b91d36c39af25efaeb9c
KirtanKapadia <kirtankapadia13@gmail.com>
2019-03-02 11:25:01 +0530

    Writing main #5

--- scheduling.py
+++ scheduling.py
@@ -25,26 +25,39 @@ class Scheduling_Algos(object):
 
     def schedule_via_fcfs(self):
         with open("FCFS.txt","w") as file:
-            for i in range(4):
+            for i in range(len(self.id)):
 
                 if i == 0:
+
                     self.completion_time.append(self.bt[i])
                     self.wt.append("0")
                     self.tat.append(self.bt[i])
-                    string = "{},{},{},{}".format(self.id[i],self.wt[i],self.completion_time[i],self.tat[i])
+
+                    string = "{},{},{},{}\n".format(self.id[i],self.wt[i],self.completion_time[i],self.tat[i])
                     file.write(string)
+
                 else:
-                    self.completion_time.append(str(int(self.bt[i-1])+int(self.bt[i])))
+                    self.completion_time.append(str(sum((int(self.bt[:i-1])+int(self.bt[i])))))
+
                     x = str(int(self.completion_time[i]) - int(self.at[i]))
                     self.tat.append(x)
+
                     y = str(int(self.tat[i]) - int(self.bt[i]))
                     self.wt.append(y)
-                    string = "{},{},{},{}".format(self.id[i],self.wt[i],self.completion_time[i],self.tat[i])
+
+                    string = "{},{},{},{}\n".format(self.id[i],self.wt[i],self.completion_time[i],self.tat[i])
                     file.write(string)
 
             w = [int(x) for x in self.wt]
             avg = statistics.mean(w)
-            file.write("mean WT:{}".format(str(avg)))
+            file.write("mean WT:{}\n".format(str(avg)))
             w = [int(x) for x in self.tat]
             avg = statistics.mean(w)
-            file.write("mean TAT:{}".format(str(avg)))
+            file.write("mean TAT:{}\n".format(str(avg)))
+
+
+
+if __name__ == "__main__":
+    s = Scheduling_Algos()
+    s.read_file()
+    s.schedule_via_fcfs()
